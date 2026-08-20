6712e315db2ed4be5f70b8292a33991ee74c8381
LucasLucianoHerrera <llucianoh@gmail.com>
2026-08-20 19:06:34 -0300

    feat: creo el modelo dim_listings_w_hosts como tabla y cambio a views dim_hosts_cleansed y dom_listings_cleansed

--- airbnb/models/dim/dim_listings_cleansed.sql
+++ airbnb/models/dim/dim_listings_cleansed.sql
@@ -1,3 +1,9 @@
+{{
+    config(
+        materialized = 'view',
+    )
+}}
+
 with src_listings as (
     select * 
     from {{ ref('src_listings')}}
@@ -6,6 +12,7 @@ with src_listings as (
 select
     listing_id,
     listing_name,
+    room_type,
     case
         when minimum_nights = 0 then 1
         else minimum_nights
